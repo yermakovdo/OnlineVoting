42ae41bbc702fb627eb3d4c8b97c1c4aaf708681
LeDfee <yermakovdo@gmail.com>
2024-04-15 21:39:29 +0200

    Update Voter.java

--- src/main/java/Voter.java
+++ src/main/java/Voter.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Voter {
+public class Voter {
+    private int voterID;
+    private String name;
+    private Election election;
+
+    public Voter(int voterID, String name) {
+        this.voterID = voterID;
+        this.name = name;
+    }
+
+    public void registerToElection(Election election) {
+        this.election = election;
+    }
+
+    public void vote(Candidate candidate) {
+        if (election != null) {
+            election.recordVote(candidate);
+        } else {
+            System.out.println("Не может проголосовать. Избиратель не зарегистрирован для участия в каких либо выборах.");
+        }
+    }
 }
