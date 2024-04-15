383b27c48c20394d54006199356f34112d2707f9
LeDfee <yermakovdo@gmail.com>
2024-04-15 21:39:08 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,27 @@
 public class Main {
+    public static void main(String[] args) {
+        VotingSystem votingSystem = new VotingSystem();
+
+        Election election = new Election();
+
+        Voter voter1 = new Voter(1, "Вова");
+        Voter voter2 = new Voter(2, "Петя");
+        votingSystem.registerVoter(voter1);
+        votingSystem.registerVoter(voter2);
+
+        Candidate candidate1 = new Candidate(101, "Кандидат А");
+        Candidate candidate2 = new Candidate(102, "Кандидат Б");
+        votingSystem.registerCandidate(candidate1);
+        votingSystem.registerCandidate(candidate2);
+
+        votingSystem.conductElection(election);
+
+        voter1.registerToElection(election);
+        voter2.registerToElection(election);
+
+        voter1.vote(candidate1);
+        voter2.vote(candidate2);
+
+        election.displayResults();
+    }
 }
