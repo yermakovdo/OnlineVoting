313cf8086194d66c0bbcc764a1fb7252f99dacac
LeDfee <yermakovdo@gmail.com>
2024-04-15 21:38:27 +0200

    Update Candidate.java

--- src/main/java/Candidate.java
+++ src/main/java/Candidate.java
@@ -1,2 +1,15 @@
 public class Candidate {
+    private int candidateID;
+    String name;
+    private Election election;
+
+    public Candidate(int candidateID, String name) {
+        this.candidateID = candidateID;
+        this.name = name;
+    }
+
+    public void registerForElection(Election election) {
+        this.election = election;
+        election.addCandidate(this);
+    }
 }
