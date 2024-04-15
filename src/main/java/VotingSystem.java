50601975307d8f29c085edd0b0cc54d70bc7524d
LeDfee <yermakovdo@gmail.com>
2024-04-15 21:39:44 +0200

    Update VotingSystem.java

--- src/main/java/VotingSystem.java
+++ src/main/java/VotingSystem.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class VotingSystem {
+import java.util.ArrayList;
+import java.util.List;
+
+public class VotingSystem {
+    private List<Voter> registeredVoters;
+    private List<Candidate> registeredCandidates;
+    private Election currentElection;
+
+    public VotingSystem() {
+        this.registeredVoters = new ArrayList<>();
+        this.registeredCandidates = new ArrayList<>();
+    }
+
+    public void registerVoter(Voter voter) {
+        registeredVoters.add(voter);
+    }
+
+    public void registerCandidate(Candidate candidate) {
+        registeredCandidates.add(candidate);
+    }
+
+    public void conductElection(Election election) {
+        this.currentElection = election;
+    }
 }
